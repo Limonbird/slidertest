8a5688751f6dc095ebd2e2f8f20e2d7f6f681ce6
Julia Karkavina <julia-barb@yandex.ru>
2020-12-15 15:39:20 +1200

    commit

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/slidertest/'
-    : '/',
+module.exports = {  
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
